88f030091d83183021ee120c247841e69089d6c8
Vitaliy Kalancha <greedisawesome777@gmail.com>
2017-01-17 16:21:17 +0200

    Added user interface and lstener to the buttons

--- app/src/main/java/com/test/bubenivanov/mytestproj/MainActivity.java
+++ app/src/main/java/com/test/bubenivanov/mytestproj/MainActivity.java
@@ -2,6 +2,9 @@ package com.test.bubenivanov.mytestproj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button Sing_in = (Button) findViewById(R.id.Sing_in);
+        Button Sing_up = (Button) findViewById(R.id.Sing_up);
+        final EditText Name=(EditText) findViewById(R.id.editText);
+        final EditText Pass=(EditText) findViewById(R.id.editText2);
+        Name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Name.setText(" ");
+
+                
+            }
+        });
+
+
+    Sing_in.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    });
+
+
+        Sing_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
+
+
+
 }
